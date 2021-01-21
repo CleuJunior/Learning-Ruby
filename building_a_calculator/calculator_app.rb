97f5b244b7bd86426f02207855758d7b1341d0a6
CleuJunior <cleu.junior@gmail.com>
2021-01-21 10:44:33 -0300

    App de soma

---
+++ building_a_calculator/calculator_app.rb
@@ -0,0 +1,8 @@
+print "Enter a number: "
+num1 = gets.chomp.to_i()
+
+print "Enter another number: "
+num2 = gets.chomp.to_i()
+
+
+puts (num1.to_s() + " + " + num2.to_s() + " = " + (num1+ num2).to_s()) 

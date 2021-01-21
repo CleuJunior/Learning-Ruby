8b3eab2f32e91c8c4f4117409509878ed89c1a21
CleuJunior <cleu.junior@gmail.com>
2021-01-21 00:21:52 -0300

    Variaveis em Ruby

---
+++ variables.rb
@@ -0,0 +1,10 @@
+character_name = "Jhon Doe"
+character_age = "32"
+
+puts ("There once was a man named " + character_name)
+puts ("He was " + character_age + " years old")
+
+character_name = "Fores"
+
+puts ("He really liked the name " + character_name)
+puts("but didn't like being " + character_age)

f0c1a4430088be0495f474832913a8a386be9d48
CleuJunior <cleu.junior@gmail.com>
2021-01-21 15:20:16 -0300

    Introducao a Hash

---
+++ arrays/intro_hash.rb
@@ -0,0 +1,31 @@
+states = {
+
+    "Pennsylvania" => "PA",
+    "New York" => "NY",
+    "Oregon" => "OR"
+
+}
+
+
+
+puts states["Pennsylvania"]
+puts "----------------"
+puts states["New York"]
+puts "----------------"
+puts states["Oregon"]
+puts "----------------"
+
+num_states = {
+    1 => "Rio de Janeiro",
+    2 => "Sao Paulo",
+    3 => "Minas Gerais"
+
+}
+
+
+puts num_states[1]
+puts "----------------"
+puts num_states[2]
+puts "----------------"
+puts num_states[3]
+puts "----------------"

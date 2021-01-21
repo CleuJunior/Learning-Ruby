e42690cc4e654b22ccf2b039efb778f987ae9ef7
CleuJunior <cleu.junior@gmail.com>
2021-01-21 00:48:57 -0300

    Usando algumas das funcoes para String

---
+++ working_with_strings/test_strings.rb
@@ -0,0 +1,32 @@
+puts "Testing\n Ruby String\n\n"
+
+phrase = "Variable String"
+
+puts phrase.upcase()
+puts phrase.downcase()
+
+puts "_____________________\n\n"
+
+phrase_2 = "    Variable String 2"
+
+puts phrase_2
+puts phrase_2.strip()
+
+puts "_____________________\n\n"
+
+
+puts phrase.length()
+puts phrase_2.length()
+
+
+puts "_____________________\n\n"
+
+
+puts phrase.include? "Variable"
+puts phrase.include? "True"
+puts phrase[0, 9]
+puts phrase.index("S")
+
+puts "_____________________\n\n"
+
+puts "hello world".upcase()

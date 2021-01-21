0c3654abe5cc83bdee2672c7e7b5d14b7a22fc5e
CleuJunior <cleu.junior@gmail.com>
2021-01-21 01:01:15 -0300

    Usando numeros e funcoes para valores

---
+++ Math_Numbers/test_numbers.rb
@@ -0,0 +1,14 @@
+num_test01 = 20
+num_test02 = -120
+num_test03 = 13.54
+num_test04 = 23.33
+
+puts ("My fav num " + num_test01.to_s)
+puts num_test02.abs()
+
+puts num_test03.round()
+puts num_test03.floor()
+puts num_test04.ceil()
+
+puts Math.log(1)
+puts Math.sqrt(25)

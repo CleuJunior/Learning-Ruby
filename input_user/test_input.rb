74bb88cd5a2956b0c2a09ad32b8ef3bf3a7e6226
CleuJunior <cleu.junior@gmail.com>
2021-01-21 10:30:04 -0300

    Entrada de dados

---
+++ input_user/test_input.rb
@@ -0,0 +1,9 @@
+print "Enter your name: "
+my_name = gets.chomp()
+
+print "Enter your age: "
+my_age = gets.chomp()
+
+
+puts ("Hello " + my_name + ", you are doing great, keep the good working ;)")
+puts ("You're " + my_age.to_s() + " years old.")

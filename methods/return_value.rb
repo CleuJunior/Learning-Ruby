0fcb984da35cd010c58dd73deaddb7a6d7b21336
CleuJunior <cleu.junior@gmail.com>
2021-01-21 17:53:09 -0300

    Retorno de valor em Metodos / Funcoes

---
+++ methods/return_value.rb
@@ -0,0 +1,13 @@
+def cube_num(num)
+    return num * num * num
+end
+
+puts cube_num(3)
+
+
+def say_hello(name, age)
+    return "Hello #{name}, you're #{age.to_s()} years old."
+end
+
+
+puts say_hello("Fredy", 28)

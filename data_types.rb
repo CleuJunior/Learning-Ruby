0b9cab7601c19f606759998a56636b8f72017dbd
CleuJunior <cleu.junior@gmail.com>
2021-01-21 00:31:32 -0300

    Tipo de dados em Ruby

---
+++ data_types.rb
@@ -0,0 +1,5 @@
+name = "John Doe" # string type variable
+age = 41          # integer type variable
+gpa = 3.21        # float type variable
+is_on = true      # boolean type variable
+flaws = nil       # set the variable to empty

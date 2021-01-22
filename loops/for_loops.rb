0cdfae609f51974982a97bb8df13636d50db211f
CleuJunior <cleu.junior@gmail.com>
2021-01-22 17:27:38 -0300

    Laco de repeticao FOR

---
+++ loops/for_loops.rb
@@ -0,0 +1,35 @@
+friends = ["Lisa", "Fernando", "Oscar", "Angela", "Andy"]
+
+
+for firend in friends
+    puts "My friend " + firend
+
+end
+
+puts "--------------------\n"
+puts "--------------------\n"
+
+friends.each do |friend|
+    puts friend
+
+end
+
+puts "--------------------\n"
+puts "--------------------\n"
+
+for index in 1..10
+    puts index
+
+end
+
+puts "--------------------\n"
+puts "--------------------\n"
+
+
+6.times do |index|
+    puts index
+
+end
+
+puts "--------------------\n"
+puts "--------------------\n"
